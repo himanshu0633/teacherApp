ddb7e12d2cbf9137c176977dfe3229301936eda8
Himanshu <himanshu@Himanshus-MacBook-Air.local>
2026-06-03 11:53:25 +0530

    push

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -95,13 +95,12 @@ export const API_ENDPOINTS = {
 
 export const COLORS = {
   text: '#6C6C6C', // Body Text
-  darkText: '#242424', // Heading / Dark Text
+  darkText: '#242424', // Heading/Dark Text
   success: '#3FD43C', // Green
   danger: '#E83939', // Red
-  primary: '#4F25C6', // Header Background / Main Brand
+  primary: '#4F25C6', // Header Background/Main Brand
   highlight: '#0098EE', // Highlight Blue
   gradientBlue: '#0481C7', // Custom blue for gradients
-
   white: '#FFFFFF',
   black: '#000000',
   border: '#E5E5E5',
